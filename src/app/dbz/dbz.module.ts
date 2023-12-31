bd592de68a36823fdb284047939d21cc7acad0b1
ErickSot <108371429+ErickSot@users.noreply.github.com>
2023-12-31 02:44:17 -0600

    added character component

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
@@ -11,7 +12,8 @@ import { ListComponent } from './components/list/list.component';
 @NgModule({
   declarations: [
     MainPageComponent,
-    ListComponent
+    ListComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule
