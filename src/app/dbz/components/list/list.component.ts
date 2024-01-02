8460272572768dbecd389bf845c63f074a149b80
ErickSot <108371429+ErickSot@users.noreply.github.com>
2024-01-02 17:12:11 -0600

    added separated components

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,11 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+import { Character } from '../../interfaces/charachter.interface';
 
 @Component({
   selector: 'dbz-list',
   standalone: false,
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default,
 })
-export class ListComponent { }
+export class ListComponent {
+
+  @Input('prueba')
+  public characterList:Character[]=[{id:uuid(),name:'trunks',power:6000}]
+
+  @Output('onDeleteIdEmitter')
+  public onDeleteId:EventEmitter<string>=new EventEmitter();
+
+  onDeleteCharacter(index:string){
+    console.log(index);
+    this.onDeleteId.emit(index);
+  }
+
+}
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/charachter.interface';
+import { v4 as uuid } from 'uuid';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+  constructor(public dbzService:DbzService){
 
+  }
 }
